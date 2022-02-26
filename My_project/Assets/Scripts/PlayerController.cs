f5c3ef17da233cbf091f28de752a33f0eb907f49
unknown <tolyan4ik27111998@gmail.com>
2022-02-26 19:17:58 +0300

    Adding jump and new assets

--- My_project/Assets/Scripts/PlayerController.cs
+++ My_project/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
    [SerializeField] private float speedX = 1f;
-
+    private bool isGround = false;
      private float horizontal = 0f;
      private Rigidbody2D rb;
    
@@ -19,5 +19,15 @@ public class PlayerController : MonoBehaviour
    }
     void FixedUpdate() {
         rb.velocity = new Vector2(horizontal*speedX*speedXMultiplier*Time.fixedDeltaTime, rb.velocity.y);
+        if(Input.GetKey(KeyCode.W) && isGround) {
+            rb.AddForce(new Vector2(0f, 500f));
+            isGround = false;
+        }
+    }
+
+    void OnCollisionEnter2D (Collision2D other) {
+        if(other.gameObject.CompareTag("Ground")) {
+            isGround = true;
+        }
     }
 }
