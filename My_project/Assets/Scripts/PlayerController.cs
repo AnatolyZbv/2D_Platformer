c10a274a6f1661205c43f3d62d39ce814d161a31
unknown <tolyan4ik27111998@gmail.com>
2022-03-05 20:46:10 +0300

    Creating Tilemap, TileCollider fix, adding platform effector

--- My_project/Assets/Scripts/PlayerController.cs
+++ My_project/Assets/Scripts/PlayerController.cs
@@ -6,22 +6,29 @@ public class PlayerController : MonoBehaviour
 {
    [SerializeField] private float speedX = 1f;
     private bool isGround = false;
+    private bool isJump = false;
      private float horizontal = 0f;
      private Rigidbody2D rb;
    
-    const float speedXMultiplier = 450f;
+    const float speedXMultiplier = 250f;
     void Start() {
         rb = GetComponent<Rigidbody2D>();
   
     }
    void Update() {
       horizontal = Input.GetAxis("Horizontal"); //-1...+1
-   }
-    void FixedUpdate() {
-        rb.velocity = new Vector2(horizontal*speedX*speedXMultiplier*Time.fixedDeltaTime, rb.velocity.y);
         if(Input.GetKey(KeyCode.W) && isGround) {
-            rb.AddForce(new Vector2(0f, 500f));
+            isJump = true;
+        }
+    }
+
+    void FixedUpdate() {
+        rb.velocity = new Vector2(horizontal * speedX * speedXMultiplier * Time.fixedDeltaTime, rb.velocity.y);
+        
+        if(isJump) {
+            rb.AddForce(new Vector2(0f, 420f));
             isGround = false;
+            isJump = false;
         }
     }
 
